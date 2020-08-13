a33b0088f4fdfb32b13aab45971e0b37dd81d139
Sam Skjonsberg <sams@allenai.org>
2020-08-13 11:26:13 -0700

    Replace santiago with a static site. (#27)
    The explorer is currently just a placeholder that redirects to
    S2. To make this easy at the time I used the original, JavaScript
    application to serve this experience. That said JavaScript
    applications require frequent maintenance due to the security
    issues often found in public packages.
    
    This PR replaces the JavaScript application with a simple, static
    HTML page that's served by NGINX.
    
    I looked into killing the deployment all together but found that
    there's still a non-trivial amount of traffic per day hitting the
    page, so I think it makes sense to keep up for now.

--- ui/src/components/PaperAbstract.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const PaperAbstract = styled.div`
-    ${({ theme }) => `
-        max-width: 80ch;
-        margin: ${theme.spacing.xs} 0 0;
-    `}
-`;
--- ui/src/components/PaperTitle.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const PaperTitle = styled.h4`
-    ${({ theme }) => `
-        margin:  0 0 ${theme.spacing.xxs};
-    `}
-`;
